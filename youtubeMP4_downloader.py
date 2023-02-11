24f402539d84af5082009ab923b3baa9dab595b9
Chiachii Liang <chiachii.liang@gmail.com>
2023-02-12 02:04:15 +0800

    Update youtubeMP4_downloader.py

--- youtubeMP4_downloader.py
+++ youtubeMP4_downloader.py
@@ -4,16 +4,16 @@ import youtube_dl
 import time
 
 now_time = datetime.datetime.now(); #設定預計執行的時間
-url = input("輸入網址：")
+url = input("ENTER URL：")
 
 file_path = 'D:\\Users\\Desktop\\Videos' 
 if not os.path.exists(file_path):
     try:
         os.mkdir(file_path)
     except:
-        print("目標資料夾已經存在")
+        print("The entered location already exists.")
         # path = os.getcwd() #取得當前路徑
-os.chdir(r'D:\\Users\\Desktop\\Videos')  # 修改當前路徑
+os.chdir(r'D:\\Users\\Desktop\\Videos')  #修改當前路徑
        
 while True:
     try:
